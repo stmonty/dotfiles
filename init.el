46ddc89ada3c2bce5ef2ebcf7cb289854b49fc30
stmonty <stmontydev@gmail.com>
2022-09-26 01:41:52 +0000

    Trying different fringes

--- init.el
+++ init.el
@@ -14,7 +14,19 @@
 (scroll-bar-mode -1) ;; Disable visible scrollbar
 (tool-bar-mode -1)   ;; Disable the toolbar
 (tooltip-mode -1)    ;; Disable the tooltips
-(set-fringe-mode 5)
+(setq default-frame-alist
+      (append (list
+	           '(min-height . 1)
+               '(height     . 45)
+	           '(min-width  . 1)
+               '(width      . 81)
+               '(vertical-scroll-bars . nil)
+               '(internal-border-width . 24)
+               '(left-fringe    . 1)
+               '(right-fringe   . 1)
+               '(tool-bar-lines . 0)
+               '(menu-bar-lines . 0))))
+
 
 (menu-bar-mode -1)
 (setq ring-bell-function 'ignore)
