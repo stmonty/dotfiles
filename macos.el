6fff2e6fc0c4558da1565fa385d3c6745f3fe3ab
stmonty <stmontydev@gmail.com>
2023-12-24 00:17:44 -0800

    fixing font size

--- macos.el
+++ macos.el
@@ -1,4 +1,4 @@
 (global-set-key [(hyper v)] 'yank)
 (global-set-key [(hyper c)] 'kill-ring-save)
 (setq mac-option-modifier 'meta)
-(set-face-attribute 'default nil :height 140)
+(set-face-attribute 'default nil :height 130)
